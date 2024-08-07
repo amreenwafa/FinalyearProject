5b59601c53f38ba9606fe930f2520e81cf37708e
Amreen wafa <wafaamreen6@gmail.com>
2024-08-07 14:43:11 +0530

    update category

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -43,11 +43,26 @@
                                          echo $_SESSION['update'];
                                          unset($_SESSION['update']);
                                      }
+
+                                     
+                                     if(isset($_SESSION['update']))
+                                     {
+                                         echo $_SESSION['update'];
+                                         unset($_SESSION['update']);
+                                     }
                          
-
-
-
-        
+                                     if(isset($_SESSION['upload']))
+                                     {
+                                         echo $_SESSION['upload'];
+                                         unset($_SESSION['upload']);
+                                     }
+                         
+                                     if(isset($_SESSION['failed-remove']))
+                                     {
+                                         echo $_SESSION['failed-remove'];
+                                         unset($_SESSION['failed-remove']);
+                                     }
+                                                  
                           ?>
                   
                           <br><br>
